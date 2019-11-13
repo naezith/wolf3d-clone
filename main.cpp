00151ec4634c90f0d1007a7244194b7cdd8070f9
naezith <naezith@gmail.com>
2019-11-13 22:33:04 +0300

    remove quads

--- main.cpp
+++ main.cpp
@@ -41,21 +41,6 @@ static double planeX = 0, planeY = 1.03; //the 2d raycaster version of camera pl
 static const int w = 1280;
 static const int h = 720;
 
-struct line_info {
-    int screen_x = -1;
-    int draw_start = -1;
-    int draw_end = -1;
-    int texture_x = -1;
-    int mapX = -1;
-    int mapY = -1;
-    int side = -1;
-};
-
-struct square_info {
-    line_info first_line;
-    line_info last_line;
-};
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(w, h), "SFML window");
@@ -69,7 +54,6 @@ int main()
 
     sf::VertexArray lines(sf::Lines, w * 2);
     sf::Clock clock;
-    bool printed_tex = false;
     while (window.isOpen()) {
         sf::Time elapsed = clock.restart();
 
@@ -114,10 +98,6 @@ int main()
             planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
             planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
         }
-
-
-        std::vector<square_info> squares;
-        line_info previous_line{};
 
         for (int x = 0, idx_vx = 0; x < w; x++, idx_vx += 2) {
             //calculate ray position and direction
@@ -202,86 +182,17 @@ int main()
             if (side == 0 && rayDirX > 0) texX = tex_width - texX - 1;
             if (side == 1 && rayDirY < 0) texX = tex_height - texX - 1;
 
-            if(!printed_tex) std::cout << "TexX: " << texX << " side: " << side << " line_height: " << lineHeight << " mapX: " << mapX << " mapY: " << mapY << std::endl;
-
-            line_info this_line{x, drawStart, drawEnd, texX, mapX, mapY, side};
-
-            // Identify squares
-            // If did not start yet, current as last
-            if(previous_line.mapX == -1) {
-                if(!printed_tex) std::cout << "New box at " << x << std::endl;
-
-                // Fill only the first line, last is unknown
-                squares.push_back(square_info{this_line});
-            }
-            // If started, and this one is lower than the last one, then it's a new square
-            else if(mapX != previous_line.mapX || mapY != previous_line.mapY || side != previous_line.side) {
-                if(!printed_tex) std::cout << "Box ended at " << previous_line.screen_x << std::endl;
-                square_info& current_square = squares.back();
-
-                // Previous was the last one, set it
-                previous_line.screen_x++;
-                current_square.last_line = previous_line;
-
-                // Set this line as first line of a new square
-                squares.push_back(square_info{this_line});
-            }
-
+            // Prepare line
             sf::Vector2f offset{(float)texture_index.x * tex_width, (float)texture_index.y * tex_height};
             lines[idx_vx].texCoords = {float(offset.x + texX), (float)offset.y};
             lines[idx_vx + 1].texCoords = {float(offset.x + texX), (float)offset.y + tex_height};
             lines[idx_vx].position = {float(x), (float) drawStart};
             lines[idx_vx + 1].position = {float(x), (float) drawEnd};
-
-            // Save this line as previous
-            previous_line = this_line;
         }
-
-        // End the last square
-        squares.back().last_line = previous_line;
-
-        if(!printed_tex) std::cout << squares.size() << std::endl;
 
         // Clear screen
         window.clear(sf::Color::Black);
-        if(!printed_tex) std::cout << "Square size: " << squares.size() << std::endl;
-        printed_tex = true;
-
-//        // Prepare Quads
-//        sf::VertexArray quads(sf::Quads, squares.size() * 4);
-//        for(std::size_t i = 0; i < squares.size(); ++i) {
-//            square_info& sq = squares[i];
-//            int idx = i * 4;
-//
-//            if(!printed_tex) std::cout << "<<< Square: " <<
-//                                       "\n first_line" <<
-//                                       "\n -- screen_x: " << sq.first_line.screen_x <<
-//                                       "\n -- draw_start: " << sq.first_line.draw_start <<
-//                                       "\n -- draw_end: " << sq.first_line.draw_end <<
-//                                       "\n -- texture_x: " << sq.first_line.texture_x <<
-//                                       "\n last_line" <<
-//                                       "\n -- screen_x: " << sq.last_line.screen_x <<
-//                                       "\n -- draw_start: " << sq.last_line.draw_start <<
-//                                       "\n -- draw_end: " << sq.last_line.draw_end <<
-//                                       "\n -- texture_x: " << sq.last_line.texture_x << std::endl;
-//
-//            sf::Vector2f offset{(float)texture_index.x * tex_width, (float)texture_index.y * tex_height};
-//            // Top Left
-//            quads[idx + 0].position = {(float)sq.first_line.screen_x, (float) sq.first_line.draw_start};
-//            quads[idx + 0].texCoords = {offset.x + (float)sq.first_line.texture_x, offset.y};
-//            // Top Right
-//            quads[idx + 1].position = {(float)sq.last_line.screen_x, (float) sq.last_line.draw_start};
-//            quads[idx + 1].texCoords = {offset.x + (float)sq.last_line.texture_x, offset.y};
-//            // Bottom Right
-//            quads[idx + 2].position = {(float)sq.last_line.screen_x, (float) sq.last_line.draw_end};
-//            quads[idx + 2].texCoords = {offset.x + (float)sq.last_line.texture_x, offset.y + (float) tex_height};
-//            // Bottom Left
-//            quads[idx + 3].position = {(float)sq.first_line.screen_x, (float) sq.first_line.draw_end};
-//            quads[idx + 3].texCoords = {offset.x + (float)sq.first_line.texture_x, offset.y + (float) tex_height};
-//        }
-
         window.draw(lines, &texture);
-        //window.draw(quads, &texture);
         window.display();
     }
     return EXIT_SUCCESS;
